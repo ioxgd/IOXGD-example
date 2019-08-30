94254f9524c137b8bc018b26d10de959f4fc8e5c
Max <max30012540@hotmail.com>
2019-08-30 12:51:45 +0700

    เพิ่มตัวอย่างใช้งาน WiFi

---
+++ HTTPGet/HTTPGet.ino
@@ -0,0 +1,23 @@
+#include <ESP32WiFi.h>
+#include <ESP32HTTP.h>
+
+void setup() {
+  delay(2000);
+  Serial.begin(115200);
+  
+  WiFi.begin();
+  WiFi.connect("e-tech good net", "987654321");
+  
+  Serial.print("Connect");
+  while(!WiFi.isConnected()) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("Connected");
+}
+
+void loop() {
+  Serial.print("HTTP Get: ");
+  Serial.println(HTTP.get("https://ioxhop.info/time.php"));
+  delay(5000);
+}
